c3d019d391d15ab81a411a5dc0873992c59c60c7
Priscilla Valciano <priscilla.valciano@hotmail.com>
2021-01-23 13:00:02 -0300

    Aula 02 finalizada
    Criação do modal com o formulario e aplicando a função de abrir e fechar o modal com JavaScript

---
+++ js/form.js
@@ -0,0 +1,17 @@
+const Modal = {
+    open(){
+        //Abrur modal
+        //Adicionar a class active ao modal
+        document
+            .querySelector('.modal-overlay')
+            .classList.add('active');
+    },
+    close(){
+        //fechar o modal
+        //remover a class active do modal
+        document
+            .querySelector('.modal-overlay')
+            .classList
+            .remove('active');
+    }
+}
